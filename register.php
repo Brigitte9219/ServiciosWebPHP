069438166fd59fbaa021210464e8cb03d02621de
brigitte <mbpadilla4@misena.edu.co>
2023-12-02 13:31:23 -0500

    Creación del registro

---
+++ register.php
@@ -0,0 +1,5 @@
+<?php
+session_start(); // Inicia o reanuda la sesión actual
+require('conexion.php'); // Contiene el archivo que contiene la conexión a la BD
+
+$message = ""; // Variable para almacenar el mensaje de la alerta
