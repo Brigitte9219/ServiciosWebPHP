5ed950e1d6e7d978e66f024c544814a3c15d8d1c
brigitte <mbpadilla4@misena.edu.co>
2023-12-02 13:27:55 -0500

    Actualizando el crud

--- crud.php
+++ crud.php
@@ -1,16 +1,14 @@
 <?php
-session_start();//Se inicia sesión
+session_start(); // Inicia o reanuda la sesión actual
 require('conexion.php');
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("Location: login.php");
     exit;
-}//Se verifica si el usuario ha iniciado sesión, en caso negativo, se dirige a login.php
+}
 
+// FUNCIONES PARA LAS OPERACIONES EN LA BASE DE DATOS
 
-//FUNCIONES PARA LAS OPERACIONES EN LA BASE DE DATOS
-
-//Consulta todas las personas de la BD
 function obtenerPersonas($conn)
 {
     $sql = "SELECT * FROM personas";
@@ -25,17 +23,13 @@ function obtenerPersonas($conn)
     return $personas;
 }
 
-
-//Inserta una nueva persona en la base de datos
-function agregarPersona($conn, $nombre, $apellido, $telefono, $correo , $password)
+function agregarPersona($conn, $nombre, $apellido, $telefono, $correo, $password)
 {
-    $sql = "INSERT INTO personas (nombre, apellido, telefono, correo , password) VALUES ('$nombre', '$apellido', '$telefono', '$correo' ,'$password')";
+    $sql = "INSERT INTO personas (nombre, apellido, telefono, correo, password) VALUES ('$nombre', '$apellido', '$telefono', '$correo', '$password')";
     $conn->query($sql);
 }
 
-
-//Actualiza la información de una persona en la base de datos
-function actualizarPersona($conn, $id, $nombre, $apellido, $telefono, $correo , $password)
+function actualizarPersona($conn, $id, $nombre, $apellido, $telefono, $correo, $password)
 {
     $sql = "UPDATE personas SET nombre=?, apellido=?, telefono=?, correo=?, password=? WHERE id=?";
     $stmt = $conn->prepare($sql);
@@ -44,8 +38,6 @@ function actualizarPersona($conn, $id, $nombre, $apellido, $telefono, $correo , 
     $stmt->close();
 }
 
-
-//Elimina una persona de la base de datos
 function eliminarPersona($conn, $id)
 {
     $sql = "DELETE FROM personas WHERE id=?";
@@ -55,26 +47,21 @@ function eliminarPersona($conn, $id)
     $stmt->close();
 }
 
-
-//Verifica si el formulario se envio por el metodo POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['agregar'])) {
-        agregarPersona($conn, $_POST['nombre'], $_POST['apellido'], $_POST['telefono'], $_POST['correo'],  $_POST['password']);
+        agregarPersona($conn, $_POST['nombre'], $_POST['apellido'], $_POST['telefono'], $_POST['correo'], $_POST['password']);
     } elseif (isset($_POST['actualizar'])) {
         actualizarPersona($conn, $_POST['id'], $_POST['nombre'], $_POST['apellido'], $_POST['telefono'], $_POST['correo'], $_POST['password']);
     } elseif (isset($_POST['eliminar'])) {
         eliminarPersona($conn, $_POST['id']);
     } elseif (isset($_POST['logout'])) {
-        // Destruir todas las variables de sesión
         session_unset();
         session_destroy();
-
-        // Redireccionar a la página de inicio de sesión
         header("Location: login.php");
         exit;
     }
 }
-//Se muestran todas las personas de la BD
+
 $personas = obtenerPersonas($conn);
 ?>
 
@@ -84,7 +71,6 @@ $personas = obtenerPersonas($conn);
     <meta charset="UTF-8">
     <title>CRUD de Personas</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
     <style>
         body {
             background-color: #f8f9fa;
@@ -136,36 +122,6 @@ $personas = obtenerPersonas($conn);
 </head>
 <body>
 
-<div class="container mt-5">
-    <form method="post" class="mt-3">
-        <h2 class="mb-3">Registrar Cuenta</h2>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="nombre">Nombre:</label>
-                <input type="text" class="form-control" id="nombre" name="nombre" required>
-            </div>
-            <div class="form-group col-md-6">
-                <label for="apellido">Apellido:</label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="telefono">Teléfono:</label>
-                <input type="number" class="form-control" id="telefono" name="telefono" required>
-            </div>
-            <div class="form-group col-md-6">
-                <label for="correo">Correo:</label>
-                <input type="email" class="form-control" id="correo" name="correo" required>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="password">Contraseña:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
-        </div>
-        <button type="submit" name="agregar" class="btn btn-primary">Agregar Cuenta</button>
-    </form>
-
     <h2 class="mt-5">Lista de Personas</h2>
     <table class="table">
         <tr>
@@ -189,7 +145,6 @@ $personas = obtenerPersonas($conn);
                     <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#actualizarModal<?= $persona['id'] ?>">
                         Actualizar
                     </button>
-
                     <form method="post" class="d-inline">
                         <input type="hidden" name="id" value="<?= $persona['id'] ?>">
                         <input type="submit" name="eliminar" value="Eliminar" class="btn btn-danger btn-sm">
@@ -238,9 +193,9 @@ $personas = obtenerPersonas($conn);
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         var passwordTds = document.querySelectorAll("td.password");
-        passwordTds.forEach(function(td) {
+        passwordTds.forEach(function (td) {
             var originalContent = td.innerHTML;
             td.innerHTML = "****";
         });
